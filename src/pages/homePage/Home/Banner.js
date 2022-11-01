49542cc379483d7319c2cb73d513cfc44040922b
Md Hasan <mdhasan8064@gmail.com>
2022-11-01 07:32:13 +0600

    Banner And folder structure

--- src/pages/homePage/Home/Banner.js
+++ src/pages/homePage/Home/Banner.js
@@ -11,9 +11,23 @@ const Banner = () => {
         <div className="carousel w-full h-[600px] rounded-2xl mb-10">
             <div id="slide1" className="carousel-item relative w-full">
                 <img src={slider1} className="w-full  object-cover" alt='/' />
+                <div className="absolute transform -translate-y-1/2 top-1/2 text-white p-24 bg-gradient-to-r from-slate-800">
+                    <div className='md:max-w-xl'>
+                        <h1 className='text-6xl font-bold leading-[70px]'>
+                            Affordable <br /> Price for Car <br /> Servicing
+                        </h1>
+                        <p className='py-7 '>There are many variations of passages of  available, But the majority have suffered alteration in some form</p>
+                        <div className=''>
+                            <a href="/" className="btn btn-outline btn-error mr-5">Discover More</a>
+                            <a href="/" className="btn btn-outline btn-error ">Letest Project</a>
+                        </div>
+                    </div>
+                </div>
                 <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-                    <a href="#slide4" className="btn btn-circle">❮</a>
-                    <a href="#slide2" className="btn btn-circle">❯</a>
+                    <div>
+                        <a href="#slide4" className="btn btn-circle  hover:bg-orange-600 hover:border-orange-600">❮</a>
+                        <a href="#slide2" className="btn btn-circle ml-3 hover:bg-orange-600 hover:border-orange-600">❯</a>
+                    </div>
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
